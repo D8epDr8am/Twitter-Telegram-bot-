497f9dd5b489edc846d796ab0cb9b9e5099ef733
D8epDr8am <43814352+D8epDr8am@users.noreply.github.com>
2023-04-07 18:55:55 +0300

    Update

--- twitter_tele_bot.py
+++ twitter_tele_bot.py
@@ -1,58 +1,99 @@
-import tweepy
-import requests
 import os
-import logging
-from telegram.ext import Updater, CommandHandler
+import requests
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import tweepy
 
-# Twitter API Keys and Tokens
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_secret = 'your_access_secret'
+# Twitter API credentials
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_key = "YOUR_ACCESS_KEY"
+access_secret = "YOUR_ACCESS_SECRET"
 
-# Telegram Bot Token
-bot_token = 'your_bot_token'
-
-# Initialize Tweepy API with Twitter Keys and Tokens
+# Authenticate Twitter API credentials
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-# Initialize Telegram Bot
-updater = Updater(token=bot_token, use_context=True)
+# Telegram Bot token
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-# Define Telegram command to send tweets
-def send_tweet(update, context):
+# Send tweets from user timeline to Telegram chat
+def send_user_timeline(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    # Extract Tweet ID from command message
-    tweet_id = context.args[0]
-    # Get tweet with specified ID
-    tweet = api.get_status(tweet_id)
-    # Extract media entities from tweet
-    media = tweet.entities.get('media', [])
-    if len(media) > 0:
-        # If media exists, send it to Telegram chat
-        for item in media:
-            # Get media URL
-            media_url = item['media_url_https']
-            # Get media file extension
-            file_extension = os.path.splitext(media_url)[1]
-            # Download media file
-            file_name = f'{tweet_id}_{item["id"]}{file_extension}'
-            response = requests.get(media_url)
-            with open(file_name, 'wb') as f:
-                f.write(response.content)
-            # Send media file to Telegram chat
-            context.bot.send_document(chat_id=chat_id, document=open(file_name, 'rb'))
-            # Delete media file from local storage
-            os.remove(file_name)
+    username = context.args[0]
+    try:
+        tweets = api.user_timeline(screen_name=username, count=10, tweet_mode='extended')
+    except tweepy.TweepError as e:
+        context.bot.send_message(chat_id=chat_id, text=f'Error: {str(e)}')
+        return
+
+    if len(tweets) > 0:
+        for tweet in tweets:
+            # Check if tweet has media files
+            if 'media' in tweet.entities:
+                for item in tweet.extended_entities['media']:
+                    media_url = item['media_url']
+                    file_extension = os.path.splitext(media_url)[1]
+                    # Download media file
+                    file_name = f'{tweet.id}_{item["id"]}{file_extension}'
+                    response = requests.get(media_url)
+                    with open(file_name, 'wb') as f:
+                        f.write(response.content)
+                    # Send media file to Telegram chat
+                    context.bot.send_document(chat_id=chat_id, document=open(file_name, 'rb'))
+                    # Delete media file from local storage
+                    os.remove(file_name)
+            else:
+                # If media does not exist, send tweet text to Telegram chat
+                context.bot.send_message(chat_id=chat_id, text=tweet.full_text)
     else:
-        # If media does not exist, send error message
-        context.bot.send_message(chat_id=chat_id, text='No media found in tweet')
+        # If tweets do not exist, send error message
+        context.bot.send_message(chat_id=chat_id, text=f'No tweets found from user {username}')
 
-# Register Telegram command handler
-dispatcher.add_handler(CommandHandler('send_tweet', send_tweet))
+# Send tweets from list to Telegram chat
+def send_list_timeline(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    list_owner = context.args[0]
+    list_slug = context.args[1]
+    try:
+        tweets = api.list_timeline(owner_screen_name=list_owner, slug=list_slug, count=10, tweet_mode='extended')
+    except tweepy.TweepError as e:
+        context.bot.send_message(chat_id=chat_id, text=f'Error: {str(e)}')
+        return
 
-# Start Telegram Bot
+    if len(tweets) > 0:
+        for tweet in tweets:
+            # Check if tweet has media files
+            if 'media' in tweet.entities:
+                for item in tweet.extended_entities['media']:
+                    media_url = item['media_url']
+                    file_extension = os.path.splitext(media_url)[1]
+                    # Download media file
+                    file_name = f'{tweet.id}_{item["id"]}{file_extension}'
+                    response = requests.get(media_url)
+                    with open(file_name, 'wb') as f:
+                        f.write(response.content)
+                    # Send media file to Telegram chat
+                context.bot.send_document(chat_id=chat_id, document=open(file_name, 'rb'))
+                # Delete media file from local storage
+                os.remove(file_name)
+        else:
+            # If media does not exist, send tweet text to Telegram chat
+            context.bot.send_message(chat_id=chat_id, text=tweet.full_text)
+else:
+    # If tweets do not exist, send error message
+    context.bot.send_message(chat_id=chat_id, text=f'No tweets found from list {list_slug} by {list_owner}')
+
+                   
+
+Telegram command handlers
+dispatcher.add_handler(CommandHandler('user_timeline', send_user_timeline))
+dispatcher.add_handler(CommandHandler('list_timeline', send_list_timeline))
+
+Start the bot
 updater.start_polling()
+updater.idle()
+
